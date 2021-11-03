443fc66ea01eac11de92e58b0a54c55de88d1c4b
tgentzsch <tgentzsch@vision.ee.ethz.ch>
2021-11-03 18:34:08 +0100

    script update + smaller image sizes

--- scripts/install_scripts.sh
+++ scripts/install_scripts.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 
-sudo cp docker_build /usr/bin/
-sudo cp docker_run /usr/bin/
-sudo cp docker_start /usr/bin/
+# remove legacy
+sudo rm -f /usr/bin/docker_run /usr/bin/docker_build /usr/bin/docker_start 
+
+sudo cp dbuild /usr/bin/
+sudo cp drun /usr/bin/
+sudo cp dstart /usr/bin/
