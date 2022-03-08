13da8715007f6593490d62765056e3492a20ea7b
Timon Gentzsch <timongentzsch@gmail.com>
2022-03-08 13:15:10 +0100

    rename jetson scripts

--- scripts/setup_launchscripts.sh
+++ scripts/setup_launchscripts.sh
@@ -8,10 +8,10 @@ sudo rm -f /usr/bin/docker_run /usr/bin/docker_build /usr/bin/docker_start
 sudo rm -f /usr/bin/drun /usr/bin/dbuild /usr/bin/dstart
 
 if  grep -qxF "# Source jetson docker scripts" "$HOME/.bashrc" ; then
-    echo "Already sourcing jetson_docker_scripts.bash"
+    echo "Already sourcing jetson_docker_scripts"
     echo "Skipping..."
 else
     echo "# Source jetson docker scripts" >> $HOME/.bashrc
-    echo "source $SCRIPT_PATH/jetson_docker_scripts.bash" >> $HOME/.bashrc
+    echo "source $SCRIPT_PATH/jetson_docker_scripts" >> $HOME/.bashrc
     echo "jetson_docker_scripts.bash added to .bashrc"
 fi
