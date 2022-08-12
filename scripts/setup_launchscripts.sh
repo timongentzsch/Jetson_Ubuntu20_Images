ee14ecbb55c94e019f152df84b02b477e7c0388a
Timon Gentzsch <timongentzsch@gmail.com>
2022-08-12 14:55:35 +0200

    optimization of ssh key passtrough as container root user

--- scripts/setup_launchscripts.sh
+++ scripts/setup_launchscripts.sh
@@ -2,16 +2,16 @@
 SCRIPT_PATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 SCRIPT_PATH=$( echo "$SCRIPT_PATH" | sed 's/ /\\ /g')
 
-# remove legacy
-sudo rm -f /usr/bin/docker_run /usr/bin/docker_build /usr/bin/docker_start
-# remove new legacy
-sudo rm -f /usr/bin/drun /usr/bin/dbuild /usr/bin/dstart
-
 if  grep -qxF "# Source jetson docker scripts" "$HOME/.bashrc" ; then
     echo "Already sourcing jetson_docker_scripts"
-    echo "Skipping..."
-else
-    echo "# Source jetson docker scripts" >> $HOME/.bashrc
-    echo "source $SCRIPT_PATH/jetson_docker_scripts" >> $HOME/.bashrc
-    echo "jetson_docker_scripts.bash added to .bashrc"
+    echo "Deleting..."
+    # get line number
+    LINE_NUMBER=$(grep -n "# Source jetson docker scripts" "$HOME/.bashrc" | cut -d: -f1)
+    # remove line and line below
+    sed -i "${LINE_NUMBER}d" "$HOME/.bashrc"
+    sed -i "${LINE_NUMBER}d" "$HOME/.bashrc"
 fi
+
+echo "# Source jetson docker scripts" >> $HOME/.bashrc
+echo "source $SCRIPT_PATH/jetson_docker_scripts && export DUMMY_SSH_CONFIG="${SCRIPT_PATH}/dummy_root_SSH_config"" >> $HOME/.bashrc
+echo "jetson_docker_scripts.bash added to .bashrc"
